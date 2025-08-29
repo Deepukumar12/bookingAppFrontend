26ebb165595ed51fff12b44b081899ac1d06d3c3
Deepu kumar <deeputhakur0986@gmail.com>
2025-08-29 13:17:51 +0530

    i added the components inside header and navbar, also i added the pages inside home, hotel and list

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+
+// import React from "react";
+
+
+import Navbar from "../../components/navbar/Navbar.jsx";
+import Header from "../../components/header/Header.jsx";
+import "./home.css"
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Header/>
+        </div>
+    );
+};
+
+export default Home;
