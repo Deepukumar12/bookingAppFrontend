9b7af0c6a35521dba32fafac4f3101c4c219d029
Deepu kumar <deeputhakur0986@gmail.com>
2025-08-30 10:26:34 +0530

    I worked on calendar part

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,71 +1,152 @@
+import { 
+  faBed, 
+  faPlane, 
+  faCar, 
+  faCalendarDays, 
+  faUser, 
+  faTaxi 
+} from "@fortawesome/free-solid-svg-icons";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./header.css";
+import { DateRange } from "react-date-range";
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { format } from "date-fns";
+import { enUS } from 'date-fns/locale';
 
-import {faBed} from "@fortawesome/free-solid-svg-icons"
-import {faPlane} from "@fortawesome/free-solid-svg-icons"
-import {faCar} from "@fortawesome/free-solid-svg-icons"
-import {faCalendarDays, faPerson} from "@fortawesome/free-solid-svg-icons"
-import {faTaxi} from "@fortawesome/free-solid-svg-icons"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import "./header.css"
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="headerContainer">
-                <div className="headerList">
-                <div className="headerListItem active">
-                    <FontAwesomeIcon icon={faBed} />
-                    <span>Stays</span>
-                </div>
+  const [openDate, setOpenDate] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
 
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faPlane} />
-                    <span>Flights</span>
-                </div>
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faCar} />
-                    <span>Car rentals</span>
-                </div>
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faBed} />
-                    <span>Attractions</span>
-                </div>
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faTaxi} />
-                    <span>Airport taxis</span>
-                </div>
-            </div>
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
 
-            <h1 className="headerTitle">A lifetime of discount? It's Genius.</h1>
-            <p className="headerDesc">Get rewarded for your travels - unlock instant savings of 10% or more with a free Bholebooking account </p>
-            <button className="headerBtn">Sign in / Register</button>
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
 
-            <div className="headerSearch">
-                <div className="headerSearchItem">
-                    <FontAwesomeIcon icon={faBed} className="headerIcon" />
-                    <input type="text"
-                    placeholder="Where are you going?"
-                     className="headerSearchInput"
-                     />
-                </div>
-                <div className="headerSearchItem">
-                    <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
-                    <span className="headerSearchText">date to date</span>
-                </div>
-                <div className="headerSearchItem">
-                    <FontAwesomeIcon icon={faPerson} className="headerIcon" />
-                    <span className="headerSearchText">2 adults 2 children 1 room</span>
-                </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
 
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
 
-                 <div className="headerSearchItem">
-                    <button className="headerBtn">Search</button>
-                </div>
-               
-            </div>
-            </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
+          </div>
         </div>
-    );
+
+        <h1 className="headerTitle">A lifetime of discount? It's Genius.</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels - unlock instant savings of 10% or more
+          with a free Bholebooking account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span 
+              onClick={() => setOpenDate(!openDate)} 
+              className="headerSearchText"
+            >
+              {`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(
+                date[0].endDate,
+                "dd/MM/yyyy"
+              )}`}
+            </span>
+            {openDate && (
+              <DateRange
+              locale={enUS}
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className="date"
+              />
+            )}
+          </div>
+
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faUser} className="headerIcon" /> 
+            <span className="headerSearchText">
+              {`${options.adult} adult . ${options.children} children . ${options.room} room`}
+            </span>
+            <div className="options">
+              
+                    <div className="optionItem">
+                        <span className="optionText">Adult</span>
+                        <div className="optionCounter">
+                        <button className="optionCounterButton">-</button>
+                        <span className="optionCounterNumber">1</span>
+                        <button className="optionCounterButton">+</button>
+                    </div>
+                </div>
+
+                <div className="optionItem">
+                        <span className="optionText">Children</span>
+                        <div className="optionCounter">
+                        <button className="optionCounterButton">-</button>
+                        <span className="optionCounterNumber">0</span>
+                        <button className="optionCounterButton">+</button>
+                    </div>
+                </div>
+
+                <div className="optionItem">
+                        <span className="optionText">Room</span>
+                        <div className="optionCounte
+                        <button className="optionCounterButton">-</button>
+                        <span className="optionCounterNumber">1</span>
+                        <button className="optionCounterButton">+</button>
+                    </div>
+                </div>
+            </div>
+          </div>
+
+          <div className="headerSearchItem">
+            <button className="headerBtn">Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
